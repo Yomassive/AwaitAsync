b65bb0575c7399b469c79a9bb8891f743818a12b
Andrew <yomassive.andrew@yandex.ru>
2018-02-21 11:32:47 +0400

    added throw errors try and catch

--- processes.js
+++ processes.js
@@ -3,20 +3,29 @@ var wait = util.promisify(setTimeout);
 
 module.exports = {
     async process01(){
-        console.log('Process 01 started');
-        console.time('Process 01 ended');
-        await wait(5000);
-        console.timeEnd('Process 01 ended');
-        console.log();
-        return 'Process01-value';
+        try{
+            console.log('Process 01 started');
+            throw new Error('Big ERROR in Process01');
+            console.time('Process 01 ended');
+            await wait(5000);
+            console.timeEnd('Process 01 ended');
+            console.log();
+            return 'Process01-value';
+        } catch(err){
+            console.error('error', err);
+        }
     },
 
     async process02(){
-        console.log('Process 02 started');
-        console.time('Process 02 ended');
-        await wait(3000);
-        console.timeEnd('Process 02 ended');
-        console.log();
-        return 'Process02-value';
+        try{
+            console.log('Process 02 started');
+            console.time('Process 02 ended');
+            await wait(3000);
+            console.timeEnd('Process 02 ended');
+            console.log();
+            return 'Process02-value';
+        } catch(err){
+            console.error('error', err);
+        }
     }    
 };
